09bec6dd8d2532da258163c29693f0188db0e7fd
Kevin McGill <kevin@mcgilldevtech.com>
2017-06-10 19:49:57 -0500

    Add licenses to all swift files.
    Add git ignore.
    Experimenting with popover.

--- Example/McPicker/ViewController.swift
+++ Example/McPicker/ViewController.swift
@@ -1,10 +1,22 @@
+//Copyright (c) 2017 Kevin McGill <kevin@mcgilldevtech.com>
 //
-//  ViewController.swift
-//  McPicker
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
 //
-//  Created by Kevin McGill on 03/22/2017.
-//  Copyright (c) 2017 Kevin McGill. All rights reserved.
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
 //
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
 
 import UIKit
 import McPicker
@@ -45,13 +57,13 @@ class ViewController: UIViewController {
         
         // Short hand
         //
-        McPicker.show(data: [["Kevin", "Lauren", "Kibby", "Stella"]], doneHandler: { selections in
+        McPicker.show(data: [["Kevin", "Lauren", "Kibby", "Stella"]]) { selections in
             // Selection(s) Made
             //
             if let name = selections[0] {
                 self.label.text = name
             }
-        })
+        }
     }
     
     @IBAction func styledPicker(_ sender: Any) {
@@ -73,11 +85,21 @@ class ViewController: UIViewController {
         picker.toolbarBarTintColor = .darkGray
         picker.pickerBackgroundColor = .gray
         
-        picker.show(doneHandler: { selections in
+        picker.show() { selections in
             
             if let prefix = selections[0], let name = selections[1] {
                 self.label.text = "\(prefix) \(name)"
             }
-        })
+        }
     }
+    
+    @IBAction func popOverPicker(_ sender: Any) {
+        
+        
+        
+        
+        
+    }
+    
+    
 }
