2e66cdc90ea48410c3f462518d8d94f8b1f57ff9
Kevin McGill <kevin@mcgilldevtech.com>
2018-03-07 23:14:40 -0600

    Update licenses

--- Example/McPicker/ViewController.swift
+++ Example/McPicker/ViewController.swift
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2017 Kevin McGill <kevin@mcgilldevtech.com>
+Copyright (c) 2017-2018 Kevin McGill <kevin@mcgilldevtech.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- Example/Tests/McPickerBarButtonItemTests.swift
+++ Example/Tests/McPickerBarButtonItemTests.swift
@@ -1,6 +1,6 @@
 /*
- Copyright (c) 2017 Kevin McGill <kevin@mcgilldevtech.com>
- 
+ Copyright (c) 2017-2018 Kevin McGill <kevin@mcgilldevtech.com>
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
